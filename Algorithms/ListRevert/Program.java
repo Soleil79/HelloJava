fba4ccbad632c524e86fbed712dba31d3e6b09b6
elena_s <esonina@icloud.com>
2023-04-13 13:12:05 +0300

    deleted errors from ListRevert

--- Algorithms/ListRevert/Program.java
+++ Algorithms/ListRevert/Program.java
@@ -19,18 +19,3 @@ public class Program{
     }
 }
 
-// public static void main(String[] args) throws IOException {
-//     Db db = new Db();
-//     WriteFile writeFile = new WriteFile(db);
-//     ChangeData changeData = new ChangeData(db, writeFile);
-//     StaffController staffController = new StaffController(db, writeFile, changeData);
-//     View view = new View(staffController);
-
-//     ReadFile ReadFile = new ReadFile();
-//     List<String> list = ReadFile.readAllLines();
-//     db.employees = ReadFile.readList(list);
-
-//     view.choices();
-    
-//     view.intScanClose();
-//     view.strScanClose();
